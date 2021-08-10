b0e334c57c0e23cb36901d0d28516c7a778f4bc1
keenanlang <klang@aps.anl.gov>
2021-08-09 22:05:13 -0500

    Updates to demonstrate new gestalt capabilities

--- generate_ui.py
+++ generate_ui.py
@@ -12,65 +12,12 @@ a_display.addChild( Gestalt.Widget("caLabel", layout=styles["clear_coloring"])
 	.setProperties( geometry="935x25", text="$(P) Common Plugins")
 	.position(0, 6) )
 
-a_display.addChild( styles["UIHeader"].position(0, 40) )
+a_display.addChild( styles["UIHeader"].position(0, 40), macros={ 'text': "Plugin Type" })
 
 y_val = 71
 
-for row in Spreadsheet.rows("data.xlsx"):
-	
-	#Plugin Name
-	a_display.addChild( Gestalt.Widget("caLineEdit", layout=styles["clear_lineedit"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":PortName_RBV")
-			.setProperty("geometry", "110x18")
-			.position(10, y_val) )
-	
-	#Plugin Type
-	a_display.addChild( Gestalt.Widget("caLineEdit", layout=styles["clear_lineedit"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":PluginType_RBV")
-			.setProperty("geometry", "160x18")
-			.position(125, y_val) )
-	
-	#NDArray Port
-	a_display.addChild( Gestalt.Widget("caTextEntry", layout=styles["blue_entry"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":NDArrayPort")
-			.position(290, y_val - 1) )
-	
-	#Enabled
-	a_display.addChild( Gestalt.Widget("caMenu", layout=styles["blue_menu"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":EnableCallbacks")
-			.position(355, y_val - 1) )
-	
-	#Enabled Readback
-	a_display.addChild( Gestalt.Widget("caLineEdit", layout=styles["gray_coloring"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":EnableCallbacks_RBV")
-			.position(440, y_val) )
-	
-	#Callback Blocking
-	a_display.addChild( Gestalt.Widget("caMenu", layout=styles["blue_menu"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":BlockingCallbacks")
-			.position(525, y_val - 1) )
-	
-	#Dropped Arrays
-	a_display.addChild( Gestalt.Widget("caLineEdit", layout=styles["clear_lineedit"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":DroppedArrays_RBV")
-			.position(610, y_val) )
-	
-	#Queue Free
-	a_display.addChild( Gestalt.Widget("caLineEdit", layout=styles["gray_coloring"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":QueueFree")
-			.position(695, y_val) )
-	
-	#Rate
-	a_display.addChild( Gestalt.Widget("caLineEdit", layout=styles["clear_lineedit"])
-			.setProperty("channel", "$(P)" + row["Instance"] + ":ArrayRate_RBV")
-			.position(780, y_val) )
-
-	
-	#Related Displays
-	a_display.addChild( Gestalt.Widget("caRelatedDisplay", layout=styles["rel_displays"])
-			.setProperties(labels=row["Instance"], files=row["related files"], args=row["args"])
-			.position(865, y_val - 1) )
-	
+for row in Spreadsheet.rows("data.csv"):	
+	a_display.addChild(styles["UIRow"].position(0, y_val), macros=row)
 	
 	y_val += 25
 	
