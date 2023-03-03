906b26129dcf92486037801bc6544082230c58ff
Keenan Lang <klang@aps.anl.gov>
2023-03-02 19:00:56 -0600

    testing out two-phase generation

--- generate_ui.py
+++ generate_ui.py
@@ -1,30 +1,35 @@
-#! /APSshare/anaconda3/x86_64/bin/python
+#! /usr/bin/python3
 
 from pprint import pprint
 
 from gestalt import *
 
+a_display = Gestalt.QtDisplay()
+
 styles = Stylesheet.parse("layout.yml")
 
-a_display = Gestalt.Display()
+data = Spreadsheet.rows("data.csv")
 
 for key, item in styles.items():
-	if type(item) is Gestalt.Widget and item.classname == "Form":
-		a_display.setProperties(item.attrs)
+	if type(item) is Gestalt.Node:
+		if item.classname == "Form":
+			a_display.setProperties(item.attrs)
+		else:
+			a_display.append(item.generateQt({}))
 
-a_display.addChild( styles["UILabel"])
-a_display.addChild( styles["UIHeader"])
+#a_display.addChild( styles["UILabel"])
+#a_display.addChild( styles["UIHeader"])
 
-y_val = styles["UIHeader"]["geometry"]["y"] + 30
+#y_val = styles["UIHeader"]["geometry"]["y"] + 30
 
-for row in Spreadsheet.rows("data.csv"):
+#for row in Spreadsheet.rows("data.csv"):
 	
-	if row["Used"] == "x":
-		a_display.addChild(styles["UIRow"].position(0, y_val), macros=row)
+#	if row["Used"] == "x":
+#		a_display.addChild(styles["UIRow"].position(0, y_val), macros=row)
 	
-		y_val += styles["UIRow"]["geometry"]["height"]
+#		y_val += styles["UIRow"]["geometry"]["height"]
 	
 	
-a_display.setProperty("geometry", Type.Rect(935, y_val))
-	
+a_display.setProperty("geometry", Type.Rect(935, 60))
+
 a_display.writeQt("commonPlugins.ui")
